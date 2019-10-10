d2a9430ecc839aee71b797c7ef042807951712e5
Courtney <lrccoordntr@aol.com>
2019-10-09 23:28:57 -0500

    Changed text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
-const Header = (props) =>
+const Header = props => (
   <header>
-    <span>Header</span>
+    <h1>Who's Who</h1>
+    <br />
   </header>
+)
 
 export default Header
+
+// import React from 'react'
+// import styled from 'styled-components'
+
+// const Span = styled.span`
+//   color: 'green';
+// `
+// const Header = props => (
+//   <header>
+//     <Span>Header</Span>
+//   </header>
+// )
+
+// export default Header
