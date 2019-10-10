442502f15e6354ac90d55d58f86ed059eafc1fa0
Courtney <lrccoordntr@aol.com>
2019-10-10 10:56:20 -0500

    Added commented out styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import styled from 'styled-components'
 
 import Header from './components/Header'
 import Home from './containers/Home'
+import Game from './containers/Game'
 
-const App = (props) => (
-  <div>
+const Div = styled.div`
+  /* background-color: #faebd7; */
+`
+
+const App = props => (
+  <Div>
     <Route path='/' component={Header} />
     <Route exact path='/' component={Home} />
-  </div>
+    <Route exact path='/game' component={Game} />
+  </Div>
 )
 
 export default App
